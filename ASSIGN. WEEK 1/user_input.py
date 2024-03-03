6c5fdc3a1598f66044ccf536fa6879391272f550
greengoose24 <balq.ali2829@gmail.com>
2024-03-03 21:40:53 +0300

    first commit

---
+++ ASSIGN. WEEK 1/user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+print("Once upon a time in a town called " + location + ",there lived a " + age +  " year old man named " + name + ".")
+
+
