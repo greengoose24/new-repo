6848086439945e1457443e07f8ad91decc45554a
greengoose24 <160274844+greengoose24@users.noreply.github.com>
2024-03-03 21:45:03 +0300

    Delete ASSIGN. WEEK 1 directory

--- ASSIGN. WEEK 1/user_input.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-age = input("What is your age? ")
-location = input("What is your location? ")
-print("Once upon a time in a town called " + location + ",there lived a " + age +  " year old man named " + name + ".")
-
-
